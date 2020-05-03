5874c55a8693541d9f2beefe7c0f4e3e02d363ac
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-05-03 17:32:50 +0530

    Step 1 - Get the HTML Done

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Web-Scraping-BeautifulSoup
 This is a Web Scraper made using BeautifulSoup.
+
+Steps to use it
+* Install python on your system
+* Install 'pip' on your system
+
+>Download required packages using following commands:
+>
+>`pip install requests`
+>
+>`pip install html5lib`
+>
+>`pip install bs4`
+
+* Run 'main.py' file
---
+++ main.py
@@ -0,0 +1,23 @@
+# Step 0:
+# Install requirements
+# pip install requests
+# pip install html5lib
+# pip install bs4
+
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.codewithharry.com/"
+
+
+# Step 1:
+# Get the HTML
+r = requests.get(url)
+htmlContent = r.content
+print(htmlContent)
+
+# Step 2:
+# Parse HTML
+
+
+# Step 3:
+# HTML Tree Traversal
